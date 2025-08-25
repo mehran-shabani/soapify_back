fe5272ccd0e2ca398e6856602250077454f70536
Dr.hossein Shabani <shabanimehran@gmail.com>
2025-08-25 14:02:50 +0330

    Update admin.py

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,3 +1,15 @@
+# upload/admin.py
 from django.contrib import admin
+from .models import AudioSession, AudioChunk
 
-# Register your models here.
+@admin.register(AudioSession)
+class AudioSessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "storage_backend", "is_committed", "created_at")
+    list_filter = ("storage_backend", "is_committed", "created_at")
+    search_fields = ("id", "s3_object_key")
+
+@admin.register(AudioChunk)
+class AudioChunkAdmin(admin.ModelAdmin):
+    list_display = ("session", "chunk_index", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("session__id",)
